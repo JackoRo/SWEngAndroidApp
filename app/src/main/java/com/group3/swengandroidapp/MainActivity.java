97e1c0d03ff4181e9d45c65f41c8f56e07953252
kevinsum <kevinsum20051996@hotmail.com>
2018-02-12 14:28:09 +0000

    Menu bar

--- app/src/main/java/com/group3/swengandroidapp/MainActivity.java
+++ app/src/main/java/com/group3/swengandroidapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.group3.swengandroidapp;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_resource,menu);
+        return true;
     }
 }
