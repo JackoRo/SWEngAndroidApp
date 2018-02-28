101d215bef3b852e42ac29482cc4820fa01ad4f3
Jack Rookes <jr1175@york.ac.uk>
2018-02-28 14:05:33 +0000

    Adding new text methods

--- app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Text.java
+++ app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Text.java
@@ -13,8 +13,14 @@ public abstract class Text extends XmlElement implements Drawable {
     public String getFont(){
         return getInheritableProperty("font");
     }
-
     public void setFont(String font){
         setProperty("font", font);
     }
+
+    public String getBold() {return getInheritableProperty("bold");}
+    public void setBold(String bold) {setProperty("bold", bold);}
+
+    public String getUnderline() {return getInheritableProperty("underline");}
+    public void setUnderline(String underline) {setProperty("underline", underline);}
+
 }
