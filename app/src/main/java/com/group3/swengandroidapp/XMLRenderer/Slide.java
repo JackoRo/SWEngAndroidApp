5b6c679c31b285a404e6ccd7e77e92f84f1e78a3
Jack Rookes <jr1175@york.ac.uk>
2018-02-28 22:52:39 +0000

    Add text options for bold, underline, and italic

--- app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Slide.java
+++ app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Slide.java
@@ -1,6 +1,7 @@
 package com.group3.swengandroidapp.XMLRenderer;
 
 import android.app.Activity;
+import android.widget.LinearLayout;
 
 /**
  * Created by Jack on 22/02/2018.
@@ -8,14 +9,27 @@ import android.app.Activity;
 
 public class Slide extends XmlElement {
 
+    private LinearLayout linearLayout;
+
     // Must call drawAll again on activity destroy e.g. rotating phone
 
     public Slide (XmlElement parent) {
         super(parent);
+
+    }
+
+    public LinearLayout getLayout() {
+        return linearLayout;
     }
 
     @Override
     public void draw(Activity activity) {
+
+        // Set layout of the slide
+        linearLayout = new LinearLayout(activity);
+        activity.setContentView(linearLayout);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
         for (Drawable item : children) {
             item.draw(activity);
         }
--- app/src/main/java/com/group3/swengandroidapp/XMLRenderer/TextAndroid.java
+++ app/src/main/java/com/group3/swengandroidapp/XMLRenderer/TextAndroid.java
@@ -2,13 +2,15 @@ package com.group3.swengandroidapp.XMLRenderer;
 
 import android.app.Activity;
 import android.graphics.Typeface;
+import android.text.Spannable;
 import android.text.SpannableStringBuilder;
-import android.text.Spanned;
 import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Jack on 23/02/2018.
@@ -16,7 +18,8 @@ import android.widget.TextView;
 
 public class TextAndroid extends Text {
 
-    private StyleSpan boldSpan = new StyleSpan(Typeface.ITALIC);
+    private StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+    private StyleSpan italicSpan = new StyleSpan(Typeface.ITALIC);
     private StyleSpan plainSpan = new StyleSpan(Typeface.NORMAL);
     private UnderlineSpan underlineSpan = new UnderlineSpan();
 
@@ -25,61 +28,108 @@ public class TextAndroid extends Text {
 
     }
 
+    private UnderlineSpan getUnderlineSpan() {
+        return new UnderlineSpan();
+    }
+
+    private StyleSpan getBoldSpan() {
+        return new StyleSpan(Typeface.BOLD);
+    }
+
+    private StyleSpan getItalicSpan() {
+        return new StyleSpan(Typeface.ITALIC);
+    }
+
+    private StyleSpan getPlainSpan() {
+        return new StyleSpan(Typeface.NORMAL);
+    }
+
     @Override
     public void draw(Activity activity) {
-        TextView textView = new TextView(activity);
 
-        // Probably make a TextView and add it to the parent view or whatever
-        LinearLayout linearLayout = new LinearLayout(activity);
-        activity.setContentView(linearLayout);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        if (parent instanceof Slide) {
+            LinearLayout layout = ((Slide) parent).getLayout();
+            TextView textView = new TextView(activity);
 
-        SpannableStringBuilder builder = new SpannableStringBuilder();
-        buildString(builder);
+            SpannableStringBuilder builder = new SpannableStringBuilder();
+            buildString(builder);
 
-        String str = builder.toString();
+            textView.setText(builder);
 
-        //for (Map.Entry<String, String> e : properties.entrySet()) {
-           // Log.d("TextAndroid", String.format("%s : %s", e.getKey(), e.getValue()));
-        //}
-
-
-        textView.setText(str);
-//
-//        SpannableStringBuilder ssb = new SpannableStringBuilder();
-//        ssb.append(getProperties());
-
-//        ssb.setSpan(plainSpan, 1, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//        ssb.setSpan(boldSpan, 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-//
-//        textView.setText(ssb);
-        linearLayout.addView(textView);
+            layout.addView(textView);
+        }
 
     }
 
     @Override
     public void buildString(SpannableStringBuilder builder) {
-        for (XmlElement e : children) {
-            if (getBold().equals("true")) {
-                    Log.d("TextAndroid: buildString: bold: ", getBold());
-                    builder.clear();
-                    builder.setSpan(boldSpan, 0, 0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-            if (getUnderline().equals("true")) {
-                    Log.d("TextAndroid: buildString: underline: ", getUnderline());
-                    builder.clear();
-                    builder.setSpan(underlineSpan,0 ,0, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
 
+        for (XmlElement e : children) {
 
             if (e instanceof RawText) {
+                int start = builder.length();
+
                 RawText t = (RawText) e;
-                Log.d("TextAndroid", t.getText());
+
+                Log.d("TextAndroid: buildString: Text", t.getText());
+
+                Log.d("TextAndroid: buildString: Bold", String.valueOf(getBold().equals("true")));
+                Log.d("TextAndroid: buildString: Italic", String.valueOf(getItalic().equals("true")));
+                Log.d("TextAndroid: buildString: Underline", String.valueOf(getUnderline().equals("true")));
 
                 builder.append(t.getText());
+
+                buildBUI(builder, start);
+                buildFont(builder);
+
             } else if (e instanceof Format) {
                 ((Format)e).buildString(builder);
             }
+
         }
     }
+
+    // sets the Bold, Underline, Italic settings
+    private void buildBUI (SpannableStringBuilder builder, int start) {
+
+        if (getBold().equals("true") && getItalic().equals("true") && getUnderline().equals("true")) {
+            builder.setSpan(getBoldSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(getItalicSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(getUnderlineSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        }
+        else if (getUnderline().equals("true") && getBold().equals("true")) {
+            builder.setSpan(getBoldSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(getUnderlineSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else if (getUnderline().equals("true") && getItalic().equals("true")) {
+            builder.setSpan(getItalicSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(getUnderlineSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else if (getBold().equals("true") && getItalic().equals("true")) {
+            builder.setSpan(getBoldSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            builder.setSpan(getItalicSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else if (getBold().equals("true")) {
+            builder.setSpan(getBoldSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else if (getItalic().equals("true")) {
+            builder.setSpan(getItalicSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else if (getUnderline().equals("true")) {
+            builder.setSpan(getUnderlineSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        else {
+            builder.setSpan(getPlainSpan(),start,builder.length(),Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+    }
+
+    //set Fonts
+    private void buildFont (SpannableStringBuilder builder) {
+
+
+
+    }
+
 }
