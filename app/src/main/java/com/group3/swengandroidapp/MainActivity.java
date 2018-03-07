7a1a9cf6786536970a0ec2bd215e3bc3b2f63cbd
Jack Rookes <jr1175@york.ac.uk>
2018-03-07 14:14:36 +0000

    Fix .gitignore to include .pyc and remove menu_main.xml

--- app/src/main/java/com/group3/swengandroidapp/MainActivity.java
+++ app/src/main/java/com/group3/swengandroidapp/MainActivity.java
@@ -12,6 +12,7 @@ import android.app.FragmentManager;
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
