23daf3b2afc80882c36d56d8df8e151939b3b38a
Alben-Soul <arb555@york.ac.uk>
2018-03-14 10:45:42 +0000

    #1 Added the gui elements for the Instructional Videos game.

---
+++ app/src/main/java/com/group3/swengandroidapp/instructional_Videos_GUI.java
@@ -0,0 +1,24 @@
+package com.group3.swengandroidapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class instructional_Videos_GUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructional__videos__gui);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+}
