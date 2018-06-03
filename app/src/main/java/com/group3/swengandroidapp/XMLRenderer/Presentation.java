bd5d53877975c059c50d99116fef62ce0c4a1f51
kevin sum <kevinsum20051996@hotmail.com>
2018-06-03 15:43:53 +0100

    Got rid of Donald Trump

--- app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Presentation.java
+++ app/src/main/java/com/group3/swengandroidapp/XMLRenderer/Presentation.java
@@ -9,6 +9,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.group3.swengandroidapp.AudioPlayer;
 import com.group3.swengandroidapp.SImpLeGraphicsModule.GraphicModuleAndroid;
