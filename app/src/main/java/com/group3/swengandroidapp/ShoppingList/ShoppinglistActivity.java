f091ed974170db7d30074f7b41ab7ee5c80a6e0f
Alex Bennett <36664775+Alben-Soul@users.noreply.github.com>
2018-06-05 17:46:17 +0100

    Update ShoppinglistActivity.java

--- app/src/main/java/com/group3/swengandroidapp/ShoppingList/ShoppinglistActivity.java
+++ app/src/main/java/com/group3/swengandroidapp/ShoppingList/ShoppinglistActivity.java
@@ -23,7 +23,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/*This is the shopping list code that runs the main GUI for the shopping list. Developed by Alex Bennett using references from;
+/*This is the shopping list code that runs the main GUI for the shopping list. Developed using references from;
     https://www.youtube.com/watch?v=3QHgJnPPnqQ&t=1545s
     https://developer.android.com/guide/topics/ui/declaring-layout#AdapterViews
     https://robgibbens.com/androids-built-in-list-item-layouts/*/
@@ -164,7 +164,7 @@ public class ShoppinglistActivity extends MainActivity {
 
             // This could be made neater by using a standardised format and parser like JSON
             // but this should be fine too.
-            Matcher m = Pattern.compile("(\\d+)=true").matcher(data4);
+            Matcher m = Pattern.compile("(\\d+)=1").matcher(data4);
 
             while (m.find()) {
                 String s = m.group();
@@ -221,6 +221,7 @@ public class ShoppinglistActivity extends MainActivity {
                             Integer updatedInteger = value1 + value2;
                             String updatedValue = Integer.toString(updatedInteger);
 
+                            //Add a new entry with the updated total quantity and remove the 2 old entries.
                             listItem newIngredientsItem = new listItem(name, updatedValue, unit1);
                             arrayListForShopping.add(newIngredientsItem);
                             itemNames.add(name);
